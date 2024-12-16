c53f98a90fb37d100a48e17baefc12f60de7c9a6
ftsoriaromano <ftsoriaromano@gmail.com>
2024-12-16 20:17:21 -0300

    last commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const formOpenBtn = document.querySelector("#form-open"),
 home = document.querySelector(".home"),
 formContainer = document.querySelector(".form_container"),
-formClose = document.querySelector(".form_close"),
+formCloseBtn = document.querySelector(".form_close"),
 signupBtn = document.querySelector("#signup"),
 loginBtn = document.querySelector("#login"),
 pwShowHide = document.querySelectorAll(".pw_hide");
